9472f4b6e63db09dd87b7bcdac7bb4f92d6a5138
Maycol <maycolperezbadilla@gmail.com>
2026-05-10 13:35:22 -0600

    udo

---
+++ src/main/java/com/user/electricchargingstations/cr/ElectricChargingStationsCR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.user.electricchargingstations.cr;
+
+/**
+ *
+ * @author mayco
+ */
+public class ElectricChargingStationsCR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
