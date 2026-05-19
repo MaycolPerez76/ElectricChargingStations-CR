72a4dbcc459ac7ccbb0b82d3430d182d293d8434
eiteljimenes <eiteljimenes@gmail.com>
2026-05-19 12:48:01 -0600

    clases modelo

---
+++ src/main/java/modelo/Estacion.java
@@ -0,0 +1,92 @@
+
+package modelo;
+
+
+public class Estacion {
+
+    private int idEstacion;
+    private String nombre;
+    private String ubicacion;
+    private int cantidadCargadores;
+    private int cargadoresOcupados;
+    private String estado;
+    private boolean requiereCita;
+
+    public Estacion() {
+    }
+
+    public Estacion(int idEstacion, String nombre, String ubicacion,
+    int cantidadCargadores, int cargadoresOcupados,
+    String estado, boolean requiereCita) {
+
+    this.idEstacion = idEstacion;
+    this.nombre = nombre;
+    this.ubicacion = ubicacion;
+    this.cantidadCargadores = cantidadCargadores;
+    this.cargadoresOcupados = cargadoresOcupados;
+    this.estado = estado;
+    this.requiereCita = requiereCita;
+    
+    }
+
+    public int getIdEstacion() {
+        return idEstacion;
+    }
+
+    public void setIdEstacion(int idEstacion) {
+        this.idEstacion = idEstacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public int getCantidadCargadores() {
+        return cantidadCargadores;
+    }
+
+    public void setCantidadCargadores(int cantidadCargadores) {
+        this.cantidadCargadores = cantidadCargadores;
+    }
+
+    public int getCargadoresOcupados() {
+        return cargadoresOcupados;
+    }
+
+    public void setCargadoresOcupados(int cargadoresOcupados) {
+        this.cargadoresOcupados = cargadoresOcupados;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public boolean isRequiereCita() {
+        return requiereCita;
+    }
+
+    public void setRequiereCita(boolean requiereCita) {
+        this.requiereCita = requiereCita;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
---
+++ src/main/java/modelo/Vehiculo.java
@@ -0,0 +1,81 @@
+
+
+package modelo;
+
+public class Vehiculo {
+
+    private int idVehiculo;
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int anno;
+    private Persona propietario;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(int idVehiculo, String placa, String marca,
+    String modelo, int anno, Persona propietario) {
+             
+    this.idVehiculo = idVehiculo;
+    this.placa = placa;
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anno = anno;
+    this.propietario = propietario;
+    
+    }
+
+    public int getIdVehiculo() {
+        return idVehiculo;
+    }
+
+    public void setIdVehiculo(int idVehiculo) {
+        this.idVehiculo = idVehiculo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public Persona getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(Persona propietario) {
+        this.propietario = propietario;
+    }
+
+    @Override
+    public String toString() {
+        return placa;
+    }
+}
