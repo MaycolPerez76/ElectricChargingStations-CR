40804d0cf75328262d854441a11595dc12b7b8c4
eiteljimenes <eiteljimenes@gmail.com>
2026-05-31 20:28:12 -0600

    Agregar controlador, repositorio y servicio de Vehiculo

---
+++ src/main/java/com/user/controller/VehiculoController.java
@@ -0,0 +1,18 @@
+package com.user.controller;
+
+import com.user.model.Vehiculo;
+
+public class VehiculoController {
+
+    public Vehiculo registrarVehiculo(Vehiculo vehiculo) {
+        return null;
+    }
+
+    public Vehiculo consultarVehiculo(int idVehiculo) {
+        return null;
+    }
+
+    public boolean eliminarVehiculo(int idVehiculo) {
+        return false;
+    }
+}
