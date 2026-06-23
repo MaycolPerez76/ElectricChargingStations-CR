3dc9673504eb71b9d7cee158fdb4c94a7a5505f1
tksvaleria <myavaleria13@gmail.com>
2026-06-22 21:15:13 -0600

    Create Reserva.java

---
+++ src/main/java/com/user/model/Reserva.java
@@ -0,0 +1,97 @@
+package com.user.model;
+
+/**
+ *
+ * @author myava
+ */
+public class Reserva {
+
+    private String idReserva;
+    private Usuario usuario;
+    private Vehiculo vehiculo;
+    private Estacion estacion;
+    private String fechaHora;
+    private String estado;
+
+    public Reserva() {
+    }
+
+    public Reserva(String idReserva,
+                   Usuario usuario,
+                   Vehiculo vehiculo,
+                   Estacion estacion,
+                   String fechaHora,
+                   String estado) {
+
+        this.idReserva = idReserva;
+        this.usuario = usuario;
+        this.vehiculo = vehiculo;
+        this.estacion = estacion;
+        this.fechaHora = fechaHora;
+        this.estado = estado;
+    }
+
+    public String getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(String idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public Estacion getEstacion() {
+        return estacion;
+    }
+
+    public void setEstacion(Estacion estacion) {
+        this.estacion = estacion;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String toDetalle() {
+        return "Reserva{" +
+                "idReserva=" + idReserva +
+                ", usuario=" + usuario.getNombreCompleto() +
+                ", vehiculo=" + vehiculo.getPlaca() +
+                ", estacion=" + estacion.getNombre() +
+                ", fechaHora=" + fechaHora +
+                ", estado=" + estado +
+                '}';
+    }
+
+    @Override
+    public String toString() {
+        return toDetalle();
+    }
+}
